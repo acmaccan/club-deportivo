f28ac577c0b9336fca63e40a036d9313eae3bc3b
Luciana Arevalo <arevalolucianadg@gmail.com>
2025-09-24 00:00:44 -0300

    chore: create button component

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.diffplug.spotless") version "6.22.0" apply false
 }
